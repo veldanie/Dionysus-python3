c8719a9397f036fa88aae1461d3116a6974c89e3
Dmitriy Morozov <dmitriy@mrzv.org>
2010-09-12 09:55:44 -0700

    Added Python bindings for DynamicPersistenceChains

---
+++ bindings/python/dionysus/__init__.py
@@ -0,0 +1,51 @@
+from    _dionysus   import *
+from    distances   import l2, ExplicitDistances, points_file
+from    zigzag      import *
+
+
+def init_with_none(self, iter, data = None):        # convenience: data defaults to None
+    self._cpp_init_(iter, data)
+
+def repr_with_data(self):
+    str = self._cpp_repr_()
+    if type(self.data) == float:
+        str += ' %f' % self.data
+    return str
+
+Simplex._cpp_init_ =    Simplex.__init__
+Simplex.__init__   =    init_with_none
+Simplex._cpp_repr_ =    Simplex.__repr__
+Simplex.__repr__   =    repr_with_data
+
+
+def data_cmp(s1, s2):
+    return cmp(s1.data, s2.data)
+
+def data_dim_cmp(s1,s2):
+    return cmp(s1.dimension(), s2.dimension()) or data_cmp(s1,s2)
+
+def vertex_dim_cmp(s1, s2):
+    return cmp(s1.dimension(), s2.dimension()) or vertex_cmp(s1, s2)
+
+# TBD: Port this into C++
+def point_iterator( point ):
+
+    yield point.x
+    yield point.y
+    if not point.data is None:
+        yield point.data
+
+Point.__iter__ = point_iterator
+
+def closure(simplices, k):
+    """Compute the k-skeleton of the closure of the list of simplices."""
+
+    res = set()
+
+    from    itertools   import combinations
+    for s in simplices:
+        for kk in xrange(1, k+2):
+            for face in combinations(s.vertices, kk):
+                res.add(Simplex(face))
+
+    return list(res)
---
+++ bindings/python/persistence-diagram.cpp
@@ -0,0 +1,95 @@
+#include<topology/persistence-diagram.h>
+#include<utilities/types.h>
+
+#include<boost/python.hpp>
+#include<boost/python/init.hpp>
+#include<boost/shared_ptr.hpp>
+#include<boost/python/stl_iterator.hpp>
+#include <boost/python/def.hpp>
+
+
+namespace bp = boost::python;
+
+
+namespace dionysus{
+namespace python{
+
+typedef bp::object Data;
+typedef PDPoint<Data> PointD;
+typedef PersistenceDiagram<Data> PersistenceDiagramD;
+
+} } //namespace dionysus::python
+
+namespace dp = dionysus::python;
+
+
+template<class PDPoint>
+RealType    get_x_coord( const PDPoint& p ){
+    return p.x( );
+}
+
+template<class PDPoint>
+RealType    get_y_coord( const PDPoint& p ){
+    return p.y( );
+}
+
+template<class PDPoint>
+bp::object    get_data( const PDPoint& p ){
+    return p.data( );
+}
+
+void export_point( ){
+
+    bp::class_<dp::PointD>("Point")
+    .def( bp::init< RealType, RealType, bp::optional<dp::Data>  >( ) )
+    .add_property( "x", &get_x_coord<dp::PointD> )
+    .add_property( "y", &get_y_coord<dp::PointD> )
+    .add_property( "data", &get_data<dp::PointD> )
+    .def( repr(bp::self) )
+    ;
+
+}
+
+
+
+template<class PersistenceDiagram, class Point>
+boost::shared_ptr<PersistenceDiagram>    init_from_points_sequence( Dimension dimension, bp::object point_sequence ){
+
+    typedef bp::stl_input_iterator<Point> PointIterator;
+
+    PointIterator beg = PointIterator( point_sequence ), end = PointIterator( );
+    // The following line is commented out until we can figure out the Evaluator class in make_point
+    //boost::shared_ptr<PersistenceDiagram> p(  new PersistenceDiagram( beg, end );
+    boost::shared_ptr<PersistenceDiagram> p(  new PersistenceDiagram( dimension ) );
+
+    for( PointIterator cur=beg;  cur!=end; cur++ )
+        (*p).push_back( *cur );
+    return p;
+
+}
+
+
+template<class PersistenceDiagram>
+Dimension get_dimension( PersistenceDiagram dgm ){
+    return dgm.dimension( );
+}
+
+template<class PersistenceDiagram>
+SizeType get_length( PersistenceDiagram dgm ){
+    return dgm.size( );
+}
+
+void export_persistence_diagram( ){
+
+    bp::class_<dp::PersistenceDiagramD>("PersistenceDiagram")
+    .def( "__init__", bp::make_constructor(  &init_from_points_sequence< dp::PersistenceDiagramD, dp::PointD > ) )
+    .def( bp::init< Dimension >( ) )
+    .def( "append", &dp::PersistenceDiagramD::push_back )
+    .add_property( "dimension", &get_dimension<dp::PersistenceDiagramD> )
+    .def( repr(bp::self) )
+    .def( "__iter__", bp::range( &dp::PersistenceDiagramD::begin, &dp::PersistenceDiagramD::end ) )
+    .def( "__len__", &get_length<dp::PersistenceDiagramD> )
+    ;
+
+    bp::def( "bottleneck_distance", bottleneck_distance<dp::PersistenceDiagramD,dp::PersistenceDiagramD> );
+}
